fe3a74e689cb344d3b9e7432262e1a622016aa3f
Tejasvi Hegde <tejasvi.hegde@one.verizon.com>
2016-11-20 17:00:45 -0500

    Fluentwait

--- TestMadi/src/main/java/com/testmadi/utils/ElementWait.java
+++ TestMadi/src/main/java/com/testmadi/utils/ElementWait.java
@@ -2,9 +2,15 @@ package com.testmadi.utils;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import com.google.common.base.Function;
 import com.testmadi.driver.WebDriverFactory;
 
 public class ElementWait {
@@ -17,8 +23,37 @@ public class ElementWait {
 	 */
 	public static WebElement waitForElement(final WebElement element)
 			throws Throwable {
-		WebDriver driver =WebDriverFactory.getDriver();
-		driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
-		return element;
+		WebDriver driver = WebDriverFactory.getDriver();
+		WebElement foundElement = null;
+		try {
+			Wait<WebDriver> fluentWait = new FluentWait<WebDriver>(driver)
+					.withTimeout(60, TimeUnit.SECONDS)
+					.pollingEvery(5, TimeUnit.SECONDS)
+					.withMessage("The element "+element+" coud not be found after searching for a minute" )
+					.ignoring(NoSuchElementException.class);
+			foundElement = fluentWait
+					.until(visibilityOfElementLocated(element));
+			return foundElement;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return foundElement;
+
+	}
+
+	/**
+	 * 
+	 * @param element
+	 * @return
+	 */
+	private static ExpectedCondition<WebElement> visibilityOfElementLocated(
+			final WebElement element) {
+		return new ExpectedCondition<WebElement>() {
+			public WebElement apply(WebDriver driver) {
+				return element.isDisplayed() ? element : null;
+			}
+		};
 	}
 }
